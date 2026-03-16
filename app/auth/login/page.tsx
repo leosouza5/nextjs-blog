66f6290176007e84a0032a49819576fa15fbb70a
Leonardo de Souza Oliveira <dev7@aproms.com.br>
2026-03-16 13:57:15 -0400

    feat: implementing better auth, login screen and logout funtionality

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,30 +1,27 @@
-"use client";
+"use client"
 
-import { signupSchema } from "@/app/schemas/auth";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field";
-import { Input } from "@/components/ui/input";
-import { authClient } from "@/lib/auth-client";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Controller, useForm } from "react-hook-form";
-
+import { loginSchema } from "@/app/schemas/auth"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import { authClient } from "@/lib/auth-client"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Controller, useForm } from "react-hook-form"
 import z from "zod"
 
-export default function SignUpPage() {
+export default function LoginPage() {
   const form = useForm({
-    resolver: zodResolver(signupSchema),
+    resolver: zodResolver(loginSchema),
     defaultValues: {
       email: "",
-      name: "",
       password: "",
     }
   })
 
-  async function onSubmit(data: z.infer<typeof signupSchema>) {
-    await authClient.signUp.email({
+  async function onSubmit(data: z.infer<typeof loginSchema>) {
+    await authClient.signIn.email({
       email: data.email,
-      name: data.name,
       password: data.password,
     })
   }
@@ -32,25 +29,12 @@ export default function SignUpPage() {
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Sign up</CardTitle>
-        <CardDescription>Create an account to get started</CardDescription>
+        <CardTitle>Login</CardTitle>
+        <CardDescription>Login to get started right away</CardDescription>
       </CardHeader>
       <CardContent>
         <form onSubmit={form.handleSubmit(onSubmit)}>
           <FieldGroup className="gap-y-4">
-            <Controller
-              name="name"
-              control={form.control}
-              render={({ field, fieldState }) => (
-                <Field>
-                  <FieldLabel>Full Name</FieldLabel>
-                  <Input {...field} aria-invalid={fieldState.invalid} placeholder="Leonardo de Souza" />
-                  {fieldState.error && (
-                    <FieldError errors={[fieldState.error]} />
-                  )}
-                </Field>
-              )}
-            />
             <Controller
               name="email"
               control={form.control}
@@ -77,7 +61,7 @@ export default function SignUpPage() {
                 </Field>
               )}
             />
-            <Button >Submit</Button>
+            <Button >Login</Button>
           </FieldGroup>
         </form>
       </CardContent>
